84a1dfc26fdf024d571c6caa05030633ef664d79
Gabriel Previato de Andrade <gabriel.andrade@lnls.br>
2019-07-02 09:42:13 -0300

    Minor tests
    Former-commit-id: 32a52289a5d03d6b879eacaea93b625fd4ce4990

--- jupy4syn/commands/xpra_scanCommand.py
+++ jupy4syn/commands/xpra_scanCommand.py
@@ -7,7 +7,7 @@ class xpra_scanCommand(ICommand):
         pass
 
     def exec(self, parameters):
-        p = subprocess.Popen(["DISPLAY=:100", "scan_gui"], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["scan_gui"], env={DISPLAY=":100"}, stdout=subprocess.PIPE)
         stdout = p.communicate()[0]
         print(stdout)
 
