9e38bd5265b7cc9f42d4c8e1b6d6f47d5fc6ee16
Gabriel Previato de Andrade <gabriel.andrade@lnls.br>
2019-07-04 09:34:30 -0300

    Minor tests
    Former-commit-id: 90e4e726f5b5e026a86c0b7ec0f1ffb407bcc86b

--- jupy4syn/Configuration.py
+++ jupy4syn/Configuration.py
@@ -43,8 +43,10 @@ class Configuration():
 
         with open("/etc/xpra/users_displays.json", "r") as file:
             data = json.load(file)
+        print(data)
 
         self.display_number = data[os.environ["JUPYTERHUB_USER"]]
+        print(self.display_number)
 
     def display(self):
         """
